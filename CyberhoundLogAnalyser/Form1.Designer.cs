7f79778b833a66d43a05aa5822ddcd1a48a0b160
Robert Brandon <rbbr@wideband.net.au>
2022-04-17 11:49:23 +1000

    Initial commit

---
+++ CyberhoundLogAnalyser/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace CyberhoundLogAnalyser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.logTextBox = new System.Windows.Forms.RichTextBox();
+            this.processButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.domainColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataInColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataOutColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataTotalColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.logTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(669, 264);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Paste the log here:";
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.Location = new System.Drawing.Point(7, 20);
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.Size = new System.Drawing.Size(656, 238);
+            this.logTextBox.TabIndex = 0;
+            this.logTextBox.Text = "";
+            // 
+            // processButton
+            // 
+            this.processButton.Location = new System.Drawing.Point(289, 282);
+            this.processButton.Name = "processButton";
+            this.processButton.Size = new System.Drawing.Size(75, 23);
+            this.processButton.TabIndex = 1;
+            this.processButton.Text = "Process";
+            this.processButton.UseVisualStyleBackColor = true;
+            this.processButton.Click += new System.EventHandler(this.processButton_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView);
+            this.groupBox2.Location = new System.Drawing.Point(12, 311);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(669, 259);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Results:";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToResizeRows = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.domainColumn,
+            this.dataInColumn,
+            this.dataOutColumn,
+            this.dataTotalColumn});
+            this.dataGridView.Location = new System.Drawing.Point(7, 19);
+            this.dataGridView.MultiSelect = false;
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersVisible = false;
+            this.dataGridView.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView.Size = new System.Drawing.Size(656, 234);
+            this.dataGridView.TabIndex = 0;
+            this.dataGridView.CellMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView_CellMouseClick);
+            this.dataGridView.CellMouseDown += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView_CellMouseDown);
+            this.dataGridView.CellMouseEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellMouseEnter);
+            // 
+            // domainColumn
+            // 
+            this.domainColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.domainColumn.HeaderText = "Domain";
+            this.domainColumn.Name = "domainColumn";
+            this.domainColumn.ReadOnly = true;
+            // 
+            // dataInColumn
+            // 
+            dataGridViewCellStyle1.Format = "N0";
+            dataGridViewCellStyle1.NullValue = null;
+            this.dataInColumn.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataInColumn.HeaderText = "Data In";
+            this.dataInColumn.Name = "dataInColumn";
+            this.dataInColumn.ReadOnly = true;
+            // 
+            // dataOutColumn
+            // 
+            dataGridViewCellStyle2.Format = "N0";
+            dataGridViewCellStyle2.NullValue = null;
+            this.dataOutColumn.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataOutColumn.HeaderText = "Data Out";
+            this.dataOutColumn.Name = "dataOutColumn";
+            this.dataOutColumn.ReadOnly = true;
+            // 
+            // dataTotalColumn
+            // 
+            dataGridViewCellStyle3.Format = "N0";
+            dataGridViewCellStyle3.NullValue = null;
+            this.dataTotalColumn.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dataTotalColumn.HeaderText = "Total Data";
+            this.dataTotalColumn.Name = "dataTotalColumn";
+            this.dataTotalColumn.ReadOnly = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 48);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.copyToolStripMenuItem.Text = "Copy Domain";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(693, 582);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.processButton);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Cyberhound Log Analyser (\"Because Cyberhound Sucks\" Edition)";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox logTextBox;
+        private System.Windows.Forms.Button processButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn domainColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataInColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataOutColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataTotalColumn;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+    }
+}
+
---
+++ CyberhoundLogAnalyser/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace CyberhoundLogAnalyser
+{
+    public partial class Form1 : Form
+    {
+        private DataGridViewRow _CurrentRow;
+
+        public Form1()
+        {
+            InitializeComponent();  
+        }
+
+        private void processButton_Click(object sender, EventArgs e)
+        {
+            dataGridView.Rows.Clear();
+
+            string dataInRegex = @"In=(\d+)";
+            string dataOutRegex = @"Out=(\d+)";
+            string urlRegex = @"URL=(.+)";
+            
+            foreach (string line in logTextBox.Lines)
+            {
+                if (line == "Done." || line == "")
+                    continue;
+
+                try
+                {
+
+                    int dataIn = Int32.Parse(Regex.Match(line, dataInRegex).Groups[1].Value);
+                    int dataOut = Int32.Parse(Regex.Match(line, dataOutRegex).Groups[1].Value);
+                    int totalData = dataIn + dataOut;
+
+                    string uri = Regex.Match(line, urlRegex).Groups[1].Value.ToString();
+                    if (!uri.Contains("http"))
+                        uri = "http://" + uri;
+                    string domain = new Uri(uri).Host;
+
+                    bool found = false;
+
+                    foreach (DataGridViewRow row in dataGridView.Rows)
+                    {
+                        if (row.Cells[0].Value != null && row.Cells[0].Value.ToString() == domain)
+                        {
+                            found = true;
+
+                            row.Cells[1].Value = (int)row.Cells[1].Value + dataIn;
+                            row.Cells[2].Value = (int)row.Cells[2].Value + dataOut;
+                            row.Cells[3].Value = (int)row.Cells[3].Value + totalData;
+
+                            break;
+                        }
+                    }
+
+                    if (!found)
+                    {
+                        int index = dataGridView.Rows.Add();
+                        dataGridView.Rows[index].Cells[0].Value = domain;
+                        dataGridView.Rows[index].Cells[1].Value = dataIn;
+                        dataGridView.Rows[index].Cells[2].Value = dataOut;
+                        dataGridView.Rows[index].Cells[3].Value = totalData;
+                    }
+                }
+                catch { }
+            }
+
+            dataGridView.Sort(dataGridView.Columns[3], ListSortDirection.Descending);
+            dataGridView.ClearSelection();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (_CurrentRow != null)
+            {
+                Clipboard.SetText(_CurrentRow.Cells[0].Value.ToString());
+            }
+        }
+
+        private void dataGridView_CellMouseEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+                _CurrentRow = dataGridView.Rows[e.RowIndex];
+            else
+                _CurrentRow = null;
+        }
+
+        private void dataGridView_CellMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            int rowIndex = dataGridView.HitTest(e.X, e.Y).RowIndex;
+
+            if (rowIndex >= 0)
+            {
+                
+            }
+        }
+
+        private void dataGridView_CellMouseDown(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                int rowSelected = e.RowIndex;
+                if (e.RowIndex != -1)
+                {
+                    dataGridView.Rows[rowSelected].Selected = true;
+                }
+            }
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+            var cms = sender as ContextMenuStrip;
+            var mousepos = Control.MousePosition;
+            if (cms != null)
+            {
+                var rel_mousePos = cms.PointToClient(mousepos);
+                if (cms.ClientRectangle.Contains(rel_mousePos))
+                {
+                    //the mouse pos is on the menu ... 
+                    //looks like the mouse was used to open it
+                    var dgv_rel_mousePos = dataGridView.PointToClient(mousepos);
+                    var hti = dataGridView.HitTest(dgv_rel_mousePos.X, dgv_rel_mousePos.Y);
+                    if (hti.RowIndex == -1)
+                    {
+                        // no row ...
+                        e.Cancel = true;
+                    }
+                }
+                else
+                {
+                    //looks like the menu was opened without the mouse ...
+                    //we could cancel the menu, or perhaps use the currently selected cell as reference...
+                    e.Cancel = true;
+                }
+            }
+        }
+    }
+}
